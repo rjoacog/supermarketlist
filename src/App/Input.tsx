386a7ae6549730c54dcfafe649f7bce4af286e4e
rjoacog <rjoacog@gmail.com>
2023-05-15 22:21:37 -0300

    fix input

--- src/App/Input.tsx
+++ src/App/Input.tsx
@@ -1,41 +1,46 @@
 import * as React from "react";
 
 import styles from "./Input.module.css";
-import {Props} from "./types/types";
+import { Props } from "./types/types";
 
-const Input: React.FC<Props> = ({setInput, items, setItems}) => {
+const Input: React.FC<Props> = ({ setInput, items, setItems }) => {
   const handleClickClose = () => {
     setInput(false);
   };
 
   const [newItems, setNewItems] = React.useState("");
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const handleNewItems = (event: any) => {
+  function handleNewItems(event: any) {
     setNewItems(event.target.value);
-  };
+  }
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const addNewItems = (event: any) => {
+  function addNewItems(event: any) {
     event.preventDefault();
     if (newItems !== "") {
       setItems([...items, newItems]);
       localStorage.setItem("items", JSON.stringify([...items, newItems]));
       setNewItems("");
     }
-  };
+  }
 
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Add item</h1>
-      <input placeholder="ingrese su item" type="text" value={newItems} onChange={handleNewItems} />
+      <input
+        placeholder="ingrese su item"
+        type="text"
+        value={newItems}
+        onChange={handleNewItems}
+      />
       <div className={styles.btns}>
         <button className={styles.btnClose} onClick={handleClickClose}>
           Close
         </button>
         <button
           className={styles.btnAdd}
-          style={newItems !== "" ? {cursor: "pointer"} : {cursor: "no-drop"}}
+          style={
+            newItems !== "" ? { cursor: "pointer" } : { cursor: "no-drop" }
+          }
           onClick={addNewItems}
         >
           Add
